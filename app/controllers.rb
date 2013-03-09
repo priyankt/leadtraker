bb96d236a5ac0c182b894916710f80a5e998fc1e
Priyank Tiwari <priti063@student.liu.se>
2013-03-09 21:14:36 +0530

    more routes

--- app/controllers.rb
+++ app/controllers.rb
@@ -52,6 +52,21 @@ Leadtraker.controllers  do
 
   end
 
+  # Logout user
+  get '/logout' do
+    user_key = env['HTTP_AUTH_KEY']
+    user = User.first(:user_key => user_key)
+    if user.nil?
+      ret = {:success => 0, :errors => ['Invalid User']}
+      status 400
+    else
+      user.update(:user_key => nil)
+      ret = {:success => 1, :errors => ''}
+      status 200
+    end
+    ret.to_json
+  end
+
   post '/register' do
       # get email, user type and passwd from params                                                                                                             
       newUser = {}
@@ -173,36 +188,67 @@ Leadtraker.controllers  do
 
   end
 
+  # return all starges for lead type id ':id'
+  get '/api/stages/:id' do
+    user_key = env['HTTP_AUTH_KEY']
+    user = User.first(:user_key => user_key)
+    if user.nil?
+      status 404
+      ret = {:success => 0, :errors => ['Invalid User']}
+    else
+      leadStages = user.leadTypes(:id => params[:id]).leadStages.all()
+      ret = leadStages
+    end
+    ret.to_json
+  end
+
+  # new lead stage
+  post '/api/stages/:lead_type_id' do
+    user_key = env['HTTP_AUTH_KEY']
+    user = User.first(:user_key => user_key)
+    if user.nil?
+      ret = {:success => 0, :errors => ['Invalid User']}
+    else
+      leadType = user.leadTypes.get(params[:lead_type_id])
+      leadStage = LeadStage.new(:name => params[:stage_name], :leadTypes => leadType)
+      if leadStage.valid?
+        leadStage.save
+        # rescue DataMapper::SaveFailureError => e
+        # logger.error e.resource.errors.inspect
+        ret = {:success => 1}
+        status 200
+      else
+        errors = user.errors.to_hash
+        ret = {:success => 0, :errors => errors}
+        status 404
+      end
+    end
+    
+    ret.to_json
+  end
+
+  # update lead stage param[:stage_name]
+  put '/api/stages/:stage_id' do
+
+  end
+
   # delete lead stage id
   delete '/api/stages/:id' do
     user_key = env['HTTP_AUTH_KEY']
     user = User.first(:user_key => user_key)
     if user.nil?
-      ret = {:success => 0, :errors => 'Invalid User'}
+      status 404
+      ret = {:success => 0, :errors => ['Invalid User']}
     else
       stage = user.leadTypes.leadStages.get(params[:id])
-      puts stage.inspect
       if stage.nil?
-        ret = {:success => 0, :errors => 'Invalid Lead Stage'}
+        status 404
+        ret = {:success => 0, :errors => ['Invalid Lead Stage']}
       else
+        status 200
         stage.destroy
         ret = {:success => 1}
       end
-    end
-    
-    ret.to_json
-
-  end
-
-  # return all sources for this user
-  get '/api/sources' do
-    user_key = env['HTTP_AUTH_KEY']
-    user = User.first(:user_key => user_key)
-    if user.nil?
-      ret = {:success => 0, :errors => ''}
-    else
-      sources = user.leadSources.all()
-      ret = {:success => 1, :errors => '', :sources => sources}
     end
     
     ret.to_json
@@ -214,37 +260,118 @@ Leadtraker.controllers  do
     user_key = env['HTTP_AUTH_KEY']
     user = User.first(:user_key => user_key)
     if user.nil?
-      ret = {:success => 0, :errors => ''}
+      ret = {:success => 0, :errors => ['Invalid User']}
+      status 404
     else
       leadTypes = user.leadTypes.all()
-      ret = {:success => 1, :errors => '', :lead_types => leadTypes}
+      ret = leadTypes
+      status 200
     end
     ret.to_json
   end
 
-  # add new lead type
+  # New lead type params[:name], params[:description]
   post '/api/types' do
     user_key = env['HTTP_AUTH_KEY']
     user = User.first(:user_key => user_key)
     if user.nil?
-      ret = {:success => 0, :errors => ''}
+      ret = {:success => 0, :errors => ['Invalid User']}
+      status 404
     else
-      lead_types = JSON.parse params[:types]
-      user.leadTypes << lead_types
+      leadtype = LeadType.new(:name => params[:name], :description => params[:description])
+      user.leadTypes << leadtype
       if user.valid?
         user.save
-        # rescue DataMapper::SaveFailureError => e
-        # logger.error e.resource.errors.inspect
-        ret = {:success => 1}
-        status 201
+        ret = {:id => leadtype.id}
+        status 200
       else
         errors = user.errors.to_hash
         ret = {:success => 0, :errors => errors}
-        status 412
+        status 400
+      end
+    end
+    
+    ret.to_json
+  end
+
+  # rename lead type params[:name], params[:description]
+  put '/api/types/:id' do
+    user_key = env['HTTP_AUTH_KEY']
+    user = User.first(:user_key => user_key)
+    if user.nil?
+      ret = {:success => 0, :errors => ['Invalid User']}
+      status 404
+    else
+      leadtype = user.leadTypes.get(params[:id])
+      if leadtype.nil?
+        ret = {:success => 0, :errors => ['Invalid Lead Type']}
+        status 404
+      else
+        success = leadtype.update(:name => params[:name], :description => params[:description])
+        if not success
+          ret = {:success => 0, :errors => ['Invalid Lead Type']}
+          status 404
+        else
+          ret = {:success => 1}
+          status 200
+        end
       end
     end
 
     ret.to_json
+  end
+
+  # return all sources for this user
+  get '/api/sources' do
+    user_key = env['HTTP_AUTH_KEY']
+    user = User.first(:user_key => user_key)
+    if user.nil?
+      ret = {:success => 0, :errors => ['Invalid User']}
+      status 404
+    else
+      sources = user.leadSources.all()
+      ret = sources
+      status 200
+    end
+    
+    ret.to_json(:exclude => [:created_at, :updated_at])
+
+  end
+
+  # new source params[:name]
+  post '/api/sources' do
+    user_key = env['HTTP_AUTH_KEY']
+    user = User.first(:user_key => user_key)
+    if user.nil?
+      ret = {:success => 0, :errors => ['Invalid User']}
+      status 404
+    else
+      source = LeadSource.new(:name => params[:name])
+      user.leadSources << source
+      if user.valid?
+        user.save
+        ret = {:id => source.id}
+        status 200
+      else
+        errors = user.errors.to_hash
+        ret = {:success => 0, :errors => errors}
+        status 400
+      end
+    end
+    
+    ret.to_json
+  end
+
+  # get email types
+  get 'api/email_types' do
+    eTypes = EmailType.all()
+    eTypes.to_json(:exclude => [:created_at, :updated_at])
+  end
+
+  # get phone types
+  get 'api/phone_types' do
+    pTypes = PhoneType.all()
+    pTypes.to_json(:exclude => [:created_at, :updated_at])
   end
 
   # Add new lead
@@ -261,40 +388,14 @@ Leadtraker.controllers  do
         # rescue DataMapper::SaveFailureError => e
         # logger.error e.resource.errors.inspect
         ret = {:success => 1}
-        status 201
+        status 200
       else
         errors = user.errors.to_hash
         ret = {:success => 0, :errors => errors}
-        status 412
+        status 400
       end
     end
 
-    ret.to_json
-  end
-
-  # return all starges for lead type id ':id'
-  get '/api/stages/:id' do
-    user_key = env['HTTP_AUTH_KEY']
-    user = User.first(:user_key => user_key)
-    if user.nil?
-      ret = {:success => 0, :errors => ''}
-    else
-      leadStages = user.leadTypes(:id => params[:id]).leadStages.all()
-      ret = {:success => 1, :errors => '', :stages => leadStages}
-    end
-    ret.to_json
-  end
-
-  # Logout user
-  get '/logout' do
-    user_key = env['HTTP_AUTH_KEY']
-    user = User.first(:user_key => user_key)
-    if user.nil?
-      ret = {:success => 0, :errors => ''}
-    else
-      user.update(:user_key => nil)
-      ret = {:success => 1, :errors => ''}
-    end
     ret.to_json
   end
 
